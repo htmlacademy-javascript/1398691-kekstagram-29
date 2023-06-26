ac072d576ed270ba866e4ae244db29eb21c49265
EkaterinaGrishel <katesobol88@gmail.com>
2023-06-26 19:37:43 +0300

    Выполнила задание 2.29 Нужно больше функций

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,12 @@
 const checkString = (string, maxLength) => (string.length <= maxLength);
 
 checkString('Проверяемая строка', 20);
-// console.log(checkString('Проверяемая строка', 20));
 // console.log(checkString('Проверяемая строка', 18));
 // console.log(checkString('Проверяемая строка', 10));
 
 const isPolidrome = (str) => (str.toLowerCase().replaceAll(' ', '') === str.replaceAll(' ', '').toLowerCase().split('').reverse().join(''));
 
 isPolidrome('Кошка');
-// console.log(isPolidrome('Кошка'));
 // console.log(isPolidrome('ШаЛаш'));
 // console.log(isPolidrome('Лёша на полке клопа нашёл '));
 
